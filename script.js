c0b9fb5ddff32048417a35256754720456f89d82
Ikenna Richard <oguejioforrichard@gmail.com>
2022-11-28 08:41:53 -1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var navLinks = document.querySelector('#nav-links');
+
+const showMenu = () => {
+    navLinks.style.right = '0';
+};
+
+const hideMenu = () => {
+    navLinks.style.right = '-200px';
+}
